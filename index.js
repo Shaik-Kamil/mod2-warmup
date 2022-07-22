59ee9b1adf154940ac3c29effb46acb42e549c9d
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-07-22 11:52:40 -0400

    added comments

--- index.js
+++ index.js
@@ -18,17 +18,20 @@ const modules = [
   "Data Structures & Algorithms",
   "Capstone Project",
 ];
-
-// forEach() is to iterate over an array similar to a for loop
-// modules.forEach((mod) => {
-//   console.log(mod);
-// });
+//! map and forEach can only be used for arrays not objects.
+//!git  forEach() is to iterate over an array similar to a for loop
+//! shorter syntax
+//! - no need for parens when one argument in callback
+//! - no need for curly brackets when function is short enough to fit on one line
+//! - return is implied when we do not have curly brackets
+// modules.forEach((mod => console.log(mod));
 
 //! similar to a for loop or for of loop
 // * for (const mod of modules) {
 // }
 
 // map() is to manipulate an existing array and save it to a new array
+//! will creat a new modified array without modifying the original array.
 const newModules = modules.map((mod, i) => `Module ${i + 1} ${mod}`);
 
-console.log(newModules)
+console.log(newModules);
