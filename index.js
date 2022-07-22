9692e8fe7edc0e208fb1446c6dccb3308ca3c62f
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-07-22 11:43:57 -0400

    mod2 prep

--- index.js
+++ index.js
@@ -2,24 +2,33 @@
 
 // setTimeout
 // setTimeout(() => {
-//     console.log('Get ready for Module 2!')
-// }, 2000)
-
-
+//   console.log("Get ready for Module 2!");
+// }, 2000);
 
 // setInterval
 // setInterval(() => {
 //     console.log('I fire every two seconds!')
 // }, 2000)
 
+const modules = [
+  "Fundamentals",
+  "FEWD",
+  "React",
+  "Full Stack Web Dev",
+  "Data Structures & Algorithms",
+  "Capstone Project",
+];
 
+// forEach() is to iterate over an array similar to a for loop
+// modules.forEach((mod) => {
+//   console.log(mod);
+// });
 
-const modules = ["Fundamentals", "FEWD", "React", "Full Stack Web Dev", "Data Structures & Algorithms", "Capstone Project"]
+//! similar to a for loop or for of loop
+// * for (const mod of modules) {
+// }
 
+// map() is to manipulate an existing array and save it to a new array
+const newModules = modules.map((mod, i) => `Module ${i + 1} ${mod}`);
 
-// forEach()
-
-
-
-// map()
-
+console.log(newModules)
